65c239e6f89c0cade3fc3195b0ce0eda073412d8
Anton Parshevskiy <45338141+antparcse@users.noreply.github.com>
2026-05-29 22:53:42 +0300

    Create signatures.go

---
+++ andviro/signatures.go
@@ -0,0 +1,35 @@
+// Package andviro represents the interface signatures implementation.
+// Implements signature parsing as PKCS#7 with content in ASN.1.
+package v1
+
+import (
+	"github.com/andviro/pkcs7"
+	"github.com/golang-mixins/signatures"
+	"golang.org/x/xerrors"
+)
+
+// Signatures defines the structure implements interfaces schedule.
+// Using structure methods without initialization with the New constructor will lead to panic.
+type Signatures struct{}
+
+// Parse - parsing signatures into the structure according to the standards required by interface.
+func (s *Signatures) Parse(data []byte) (*signatures.Signature, error) {
+	sign, err := pkcs7.Parse(data)
+	if err != nil {
+		err = xerrors.Errorf("signature parsing error as 'PKCS #7': %w", err)
+		return nil, err
+	}
+
+	return &signatures.Signature{
+		Content:      data,
+		Certificates: sign.Certificates,
+	}, nil
+}
+
+// Ext - provides an extension for signature, according to the requirements of the interface.
+func (s *Signatures) Ext() string {
+	return ".p7s"
+}
+
+// New - constructor Signatures.
+func New() *Signatures { return &Signatures{} }
